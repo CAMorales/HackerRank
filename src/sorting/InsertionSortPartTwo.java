7bf93b9cd711079d750942acd59853105c2ea80d
Claudio Morales <claudioantonio.morales11@gmail.com>
2016-04-11 19:45:22 -0500

    Initial commit

---
+++ src/sorting/InsertionSortPartTwo.java
@@ -0,0 +1,49 @@
+package sorting;
+
+import java.util.Scanner;
+
+/**
+ * Created by Claudio Morales on 11/04/2016.
+ */
+public class InsertionSortPartTwo {
+    public static void insertionSortPart2(int[] ar)
+    {
+        // Fill up the code for the required logic here
+        // Manipulate the array as required
+        // The code for Input/Output is already provided
+        for(int i=1;i<ar.length;i++){
+            int value = ar[i];
+            int j=i-1;
+            while(j>=0 && ar[j]>value){
+                ar[j+1]=ar[j];
+                --j;
+            }
+            ar[j+1]=value;
+            printArray(ar);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt();
+        }
+        insertionSortPart2(ar);
+
+    }
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
+
+/*
+9
+9 8 7 6 5 4 3 2 1
+ */
---
+++ src/strings/MakeItAnagram.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by Claudio Morales on 11/04/2016.
+ */
+public class MakeItAnagram {
+    public static void main(String[] args) {
+        //int countLetters[] = new int[26];
+        Scanner in=new Scanner(System.in);
+        char[] firstString=in.next().toCharArray();
+        char[] secondString=in.next().toCharArray();
+        String res;
+        int countLetters[] = new int[26];
+        int count=0;
+        for(int i=0;i<firstString.length;++i){
+            countLetters[firstString[i]-'a']++;
+        }
+        for(int i=0;i<secondString.length;++i){
+            countLetters[secondString[i]-'a']--;
+        }
+        for (int i = 0; i < countLetters.length; i++) {
+            count+=Math.abs(countLetters[i]);
+        }
+        System.out.println(count);
+    }
+}

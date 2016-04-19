eef24929e6d0870c326cb083b9bbde55f3ce8548
Claudio Morales <claudioantonio.morales11@gmail.com>
2016-04-19 09:34:59 -0500

    new solutions added

--- src/com/morac/ProjectEuler/LargestPrimeFactor.java
+++ src/com/morac/ProjectEuler/LargestPrimeFactor.java
@@ -6,11 +6,56 @@ import java.util.Scanner;
  * Created by Claudio Morales on 18/04/2016.
  */
 public class LargestPrimeFactor {
+
+    private static long largestPrimeFactor(long n){
+        long i=2;
+        long lpf=2;
+        long temp=n;
+        /*while (i*i<=n){
+            while(n%i==0){
+                lpf=i;
+                n/=i;
+            }
+            ++i;
+        }
+        if (n>lpf)
+            lpf=n;
+        return lpf;
+        */
+
+        while(temp%i==0){
+            lpf=i;
+            temp/=i;
+        }
+        if (temp!=1){
+            i=3;
+            while(i<=Math.sqrt(temp)){
+                if (temp%i!=0){
+                    i+=2;
+                }else {
+                    temp/=i;
+                    i=3;
+                }
+            }
+            if (temp>2)
+                lpf=temp;
+            else
+                lpf=i;
+        }
+        return lpf;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t= in.nextInt();
+        long n;
         while(t-->0){
-            t=00;
+            n = in.nextLong();
+            System.out.println(largestPrimeFactor(n));
         }
     }
 }
+/*
+1
+600851475143
+*/

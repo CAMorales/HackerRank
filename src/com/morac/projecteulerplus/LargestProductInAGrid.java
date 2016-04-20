b2dbd31d015f836da7cfbd6cf94d54b1f6a6c75e
Claudio Morales <claudioantonio.morales11@gmail.com>
2016-04-20 16:38:56 -0500

    New solutions added

---
+++ src/com/morac/projecteulerplus/LargestProductInAGrid.java
@@ -0,0 +1,59 @@
+package com.morac.projecteulerplus;
+
+import java.util.Scanner;
+
+/**
+ * Created by Claudio Morales on 20/04/2016.
+ */
+public class LargestProductInAGrid {
+    public static void main(String[] args) {
+        int n=20;
+        int qtyOfNum=4;
+        long mat[][]=new long[n][n];
+        long temp;
+        long bigProduct=0;
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j]=in.nextInt();
+            }
+        }
+        int i,j,k;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if(j<=n-qtyOfNum){
+                    temp=mat[i][j];
+                    for (k = 1; k < qtyOfNum; k++) {
+                        temp*=mat[i][j+k];
+                    }
+                    bigProduct = temp>bigProduct?temp:bigProduct;
+                }
+
+                if (i<=n-qtyOfNum){
+                    temp=mat[i][j];
+                    for (k = 1; k < qtyOfNum; k++) {
+                        temp*=mat[i+k][j];
+                    }
+                    bigProduct = temp>bigProduct?temp:bigProduct;
+                }
+
+                if (j>=qtyOfNum && i<=n-qtyOfNum){
+                    temp=mat[i][j];
+                    for (k = 1; k < qtyOfNum; k++) {
+                        temp*=mat[i+k][j-k];
+                    }
+                    bigProduct = temp>bigProduct?temp:bigProduct;
+                }
+                if (j<=n-qtyOfNum &&i<=n-qtyOfNum){
+                    temp=mat[i][j];
+                    for (k = 1; k < qtyOfNum; k++) {
+                        temp*=mat[i+k][j+k];
+                    }
+                    bigProduct = temp>bigProduct?temp:bigProduct;
+                }
+            }
+        }
+        System.out.println(bigProduct);
+    }
+}
---
+++ src/com/morac/projecteulerplus/SummationOfPrimes.java
@@ -0,0 +1,47 @@
+package com.morac.projecteulerplus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Claudio Morales on 20/04/2016.
+ */
+public class SummationOfPrimes {
+    private static List<Integer> primos = new ArrayList<>(78500);
+
+    private static void generatePrimes(int n){
+        boolean marcados[]=new boolean[n+1];
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!marcados[i]){
+                for (int j = i*2; j <= n; j+=i) {
+                    marcados[j]=true;
+                }
+            }
+        }
+        for (int i = 2; i < marcados.length; i++) {
+            if (!marcados[i]){
+                primos.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        generatePrimes(1000000);
+        int n;
+        long res;
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0){
+            n=in.nextInt();
+            res=0;
+            for(Integer num: primos){
+                if (num>n){
+                    break;
+                }
+                res+=num;
+            }
+            System.out.println(res);
+        }
+    }
+}

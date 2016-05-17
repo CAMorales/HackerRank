55f0b5cb6c3e3fe1422ca03416d367bb7b7d7de3
Claudio Morales <claudioantonio.morales11@gmail.com>
2016-05-17 12:19:48 -0500

    New solutions added

--- src/com/morac/algorithms/strings/SherlockAndAnagrams.java
+++ src/com/morac/algorithms/strings/SherlockAndAnagrams.java
@@ -1,5 +1,8 @@
 package com.morac.algorithms.strings;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -9,10 +12,34 @@ public class SherlockAndAnagrams {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int countLetters[] = new int[26];
         int t=in.nextInt();
+        int count;
         while (t-->0){
-
+            count=0;
+            String s = in.next();
+            int len = s.length();
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < len; i++) {
+                for (int j = i; j < len; j++) {
+                    char sub[] = s.substring(i,j+1).toCharArray();
+                    Arrays.sort(sub);
+                    String s1 = String.valueOf(sub);
+                    if (map.containsKey(s1)){
+                        map.put(s1,map.get(s1)+1);
+                    }else{
+                        map.put(s1,1);
+                    }
+                }
+            }
+            int val;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (map.get(entry.getKey())>1){
+                    val=map.get(entry.getKey());
+                    val=val*(val-1)/2;
+                    count+=val;
+                }
+            }
+            System.out.println(count);
         }
     }
 }
